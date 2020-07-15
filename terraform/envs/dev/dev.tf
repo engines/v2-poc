8fb086f3c04ced504fe86381229a0d06a060b15d
Richard Heycock <rgh@filterfish.org>
2020-07-15 10:27:31 +1000

    Add consul provider & three consul containers.

--- terraform/envs/dev/dev.tf
+++ terraform/envs/dev/dev.tf
@@ -8,10 +8,37 @@ provider "lxd" {
 }
 
 
+provider "consul" {
+  address    = "[fd61:d025:74d7:f46a:216:3eff:fe0f:ec40]:8500"
+  datacenter = "dh"
+}
+
+
 module "turtle-container" {
   source  = "./modules/turtle-container"
   name    = "ns"
   image   = "engines/beowulf/base/20200623/1143/ac"
+}
+
+
+module "consul1" {
+  source  = "./modules/turtle-container"
+  name    = "consul1"
+  image   = "engines/beowulf/base/20200701/0710"
+}
+
+
+module "consul2" {
+  source  = "./modules/turtle-container"
+  name    = "consul2"
+  image   = "engines/beowulf/base/20200701/0710"
+}
+
+
+module "consul3" {
+  source  = "./modules/turtle-container"
+  name    = "consul3"
+  image   = "engines/beowulf/base/20200701/0710"
 }
 
 # ------------------------------------------------------------
